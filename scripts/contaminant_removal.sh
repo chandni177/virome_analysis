5f6725c069013936587ba7bfc5e1a3d4dea3c972
Chandni <chandni177@gmail.com>
2018-10-26 14:15:51 -0500

    Update scripts/contaminant_removal.sh

--- scripts/contaminant_removal.sh
+++ scripts/contaminant_removal.sh
@@ -17,6 +17,8 @@
 CONPATH=/mnt/data1/databases/contaminants
 HOSTPATH=/mnt/data1/databases/macaca_mulatta_ensemble
 
+mkdir run_logs/
+
 # Set file names
 for i in *_R1.fastq.gz; do
 	F=`basename $i _R1.fastq.gz`;
@@ -25,9 +27,11 @@ for i in *_R1.fastq.gz; do
 bbduk.sh in="$F"_R1.fastq.gz in2="$F"_R2.fastq.gz \
 	ref=$CONPATH/primerB.fa \
 	out="$F"_R1.s1.out.fastq out2="$F"_R2.s1.out.fastq \
-	k=16 hdist=1 mink=11 ktrim=l restrictleft=20\
+	k=16 hdist=1 mink=11 ktrim=l restrictleft=20 \
 	removeifeitherbad=f ow=t \
-	rcomp=f;
+	rcomp=f \
+	stats="$F"_step1.stats \
+	&> run_logs/"$F"_step1.log
 
 # Step 2: Remove read through. Reverse complement of primerB + 6 bases of the adapter
 bbduk.sh in="$F"_R1.s1.out.fastq in2="$F"_R2.s1.out.fastq \
@@ -35,7 +39,9 @@ bbduk.sh in="$F"_R1.s1.out.fastq in2="$F"_R2.s1.out.fastq \
 	out="$F"_R1.s2.out.fastq out2="$F"_R2.s2.out.fastq \
 	k=16 hdist=1 mink=11 ktrim=r \
         removeifeitherbad=f ow=t \
-	rcomp=f;
+	rcomp=f \
+	stats="$F"_step2.stats \
+	&> run_logs/"$F"_step2.log
 
 # Step 3: Remove additional (primer free) adapter contamination
 bbduk.sh in="$F"_R1.s2.out.fastq in2="$F"_R2.s2.out.fastq \
@@ -43,41 +49,51 @@ bbduk.sh in="$F"_R1.s2.out.fastq in2="$F"_R2.s2.out.fastq \
         out="$F"_R1.s3.out.fastq out2="$F"_R2.s3.out.fastq \
         k=16 hdist=1 mink=11 ktrim=r \
         removeifeitherbad=f ow=t \
-        rcomp=t;
+        rcomp=t \
+	stats="$F"_step3.stats \
+	&> run_logs/"$F"_step3.log
 
 # Step 4: Remove any remaining sequence which contains any primer in either orientation
 bbduk.sh in="$F"_R1.s3.out.fastq in2="$F"_R2.s3.out.fastq \
 	ref=$CONPATH/rc_primerB_ad6.fa \
 	out="$F"_R1.s4.out.fastq out2="$F"_R2.s4.out.fastq \
 	k=16 ow=t hdist=0 \
-	rcomp=t;
+	rcomp=t \
+	stats="$F"_step4.stats \
+	&> run_logs/"$F"_step4.log
 
 # Step 5: PhiX Removal
 bbduk.sh in="$F"_R1.s4.out.fastq in2="$F"_R2.s4.out.fastq \
 	ref=$CONPATH/phix174_ill.ref.fa.gz \
 	out="$F"_R1.s5.out.fastq out2="$F"_R2.s5.out.fastq \
-	k=31 hammingdistance=1 ow=t;
+	k=31 hammingdistance=1 ow=t \
+	stats="$F"_step5.stats \
+	&> run_logs/"$F"_step5.log
 
 # Step 6: Host-removal
 bbmap.sh in="$F"_R1.s5.out.fastq in2="$F"_R2.s5.out.fastq \
 	out="$F"_unmapped.s6.out.fastq outm="$F"_hostmapped.s6.out.fastq \
 	minid=0.95 maxindel=3 bwr=0.16 bw=12 minhits=2\
 	quickmatch fast \
-	path=$HOSTPATH ow=t;
+	path=$HOSTPATH ow=t \
+	&> run_logs/"$F"_step6.log
 
 # Step 7: QC contaminant removed sequences
 bbduk.sh in="$F"_unmapped.s6.out.fastq \
 	out="$F"_R1.s7.out.fastq out2="$F"_R2.s7.out.fastq outs="$F"_singletons.s7.out.fastq \
 	qtrim=r trimq=30 \
 	maxns=2 minlength=50 \
-	ow=t;
+	ow=t \
+	stats="$F"_step7.stats \
+	&> run_logs/"$F"_step7.log
 
 # Step 8: Merge read pairs
 bbmerge.sh in1="$F"_R1.s7.out.fastq in2="$F"_R2.s7.out.fastq \
 	out="$F"_merged.fastq outu1="$F"_R1.unmerged.fastq outu2="$F"_R2.unmerged.fastq \
 	rem k=62 extend2=50 ecct vstrict=t \
 	-Xmx128g \
-	ow=t;
+	ow=t \
+	&> run_logs/"$F"_step8.log
 
 # Step 9: Read repair
 	grep -A 3 '1:N:' "$F"_singletons.s7.out.fastq | sed '/^--$/d' > "$F"_singletons_R1.out.fastq;
